02e9ae11fb40f10457e51a07f3ec93aadb34dac8
treichen <treichen@Ubuntu.myguest.virtualbox.org>
2023-04-25 15:22:10 -0400

    Finished adding demos

--- expo_demo/expo_demo.c
+++ expo_demo/expo_demo.c
@@ -27,78 +27,10 @@ void draw_block(uint16_t* data, uint8_t r, uint8_t g, uint8_t b) {
     data[2] = 1;
 }
 
-// Offset value for sinusoidal demo
+// Offset value for animated demos
 static uint16_t i = 0;
 
-// Set block size for checkerboard demo based on potentiometer input
-uint16_t setBlockSize() {
-    adc_select_input(0);
-    uint16_t pot_raw = adc_read();
-    uint8_t pot = round(5 * (float)pot_raw / (1 << 12));
-
-    switch(pot) {
-        case 1: 
-            return 64;
-        case 2: 
-            return 32;
-        case 3: 
-            return 16;
-        case 4: 
-            return 8;
-        case 5: 
-            return 4;
-        default: // Input = 0 
-            return 128;
-    }
-}
-
-// Determine if square in checkerboard demo should be black
-bool blackSquare(uint16_t x, uint16_t y, uint16_t block_size) {
-    return (y % (2*block_size) < block_size && x % (block_size / 2) < (block_size/4)) || (y % (2*block_size) >= block_size && x % (block_size / 2) >= (block_size/4));
-}
-
-// Write pixel data for checkerboard demo
-void draw_checkerboard(scanvideo_scanline_buffer_t *buffer) {
-
-    uint16_t width = vga_mode.width;
-    uint16_t w_blocks = width/4;
-    uint16_t height = vga_mode.height;
-
-    uint16_t y = scanvideo_scanline_number(buffer->scanline_id);
-
-    uint16_t *p = (uint16_t *) buffer->data;
-
-    uint16_t block_size = setBlockSize();
-
-    for(int x = 0; x < w_blocks; x++) {
-        if(blackSquare(x, y, block_size)) {
-            draw_block(p, 0, 0, 0);
-        } else {
-            draw_block(p, 0x1f, 0x1f, 0x1f);
-        }
-        p += 3;
-    }
-
-    // 32 * 3, so we should be word aligned
-    assert(!(3u & (uintptr_t) p));
-
-    // Black pixel to end line (required to prevent color from bleeding into blanking)
-    *p++ = COMPOSABLE_RAW_1P;
-    *p++ = 0;
-    // End of line with alignment padding
-    // Use COMPOSABLE_EOL_ALIGN if number of tokens used is odd, COMPOSABLE_EOL_SKIP_ALIGH if even
-    *p++ = COMPOSABLE_EOL_SKIP_ALIGN;
-    *p++ = 0;
-
-    // Set number of words used and check if it exceeds buffer size
-    buffer->data_used = ((uint32_t *) p) - buffer->data;
-    assert(buffer->data_used < buffer->data_max);
-
-    // Set buffer status to be ready for use
-    buffer->status = SCANLINE_OK;
-}
-
-// Functions to update speed of sinusoidal demo based on potentiometer input
+// Functions to update speed of animated demos on potentiometer input
 uint8_t speedFrame() {
     adc_select_input(0);
     uint16_t pot_raw = adc_read();
@@ -147,12 +79,155 @@ uint8_t speedInc() {
     }
 }
 
+// Write pixel data for test pattern demo
+// Modified version of https://github.com/raspberrypi/pico-playground/blob/master/scanvideo/test_pattern/test_pattern.c 
+void draw_pattern(scanvideo_scanline_buffer_t *buffer) {
+    // figure out 1/32 of the color value
+    uint line_num = scanvideo_scanline_number(buffer->scanline_id);
+    uint32_t primary_color = 1u + (line_num * 7 / vga_mode.height);
+
+    adc_select_input(0);
+    uint16_t pot_raw = adc_read();
+    uint8_t pot = round(0x1f * (float)pot_raw / (1 << 12));
+
+    uint32_t color_mask = PICO_SCANVIDEO_PIXEL_FROM_RGB5(0x1f * (primary_color & 1u), 0x1f * ((primary_color >> 1u) & 1u), 0x1f * ((primary_color >> 2u) & 1u));
+    uint bar_width = vga_mode.width / 32;
+
+    uint16_t *p = (uint16_t *) buffer->data;
+
+    for (uint bar = 0; bar < 32; bar++) {
+        *p++ = COMPOSABLE_COLOR_RUN;
+        uint32_t color = PICO_SCANVIDEO_PIXEL_FROM_RGB5(bar & pot, bar & pot, bar & pot);
+        *p++ = color & color_mask;
+        *p++ = bar_width - 3;
+    }
+
+    // 32 * 3, so we should be word aligned
+    assert(!(3u & (uintptr_t) p));
+
+    // black pixel to end line
+    *p++ = COMPOSABLE_RAW_1P;
+    *p++ = 0;
+    // end of line with alignment padding
+    *p++ = COMPOSABLE_EOL_SKIP_ALIGN;
+    *p++ = 0;
+
+    buffer->data_used = ((uint32_t *) p) - buffer->data;
+    assert(buffer->data_used < buffer->data_max);
+
+    buffer->status = SCANLINE_OK;
+}
+
+// Write pixel data for box demo
+void draw_box(scanvideo_scanline_buffer_t *buffer) {
+
+    uint16_t w_blocks = vga_mode.width/4;
+    uint16_t height = vga_mode.height;
+    uint16_t y = scanvideo_scanline_number(buffer->scanline_id);
+
+    uint16_t *p = (uint16_t *) buffer->data;
+
+    if(i > 2*w_blocks/3) {
+        i = 0;
+    }
+
+    for(int x = 0; x < w_blocks; x++) {
+        if((y >= height/3 && y <= 2*height/3) && (x >= i && x <= ((w_blocks / 3) + i))) {
+            draw_block(p, 0x1f, 0x1f, 0);
+        } else {
+            draw_block(p, 0x1f, 0, 0x1f);
+        }
+        p += 3;   
+    }
+    
+    // 32 * 3, so we should be word aligned
+    assert(!(3u & (uintptr_t) p));
+
+    // Black pixel to end line (required to prevent color from bleeding into blanking)
+    *p++ = COMPOSABLE_RAW_1P;
+    *p++ = 0;
+    // End of line with alignment padding
+    // Use COMPOSABLE_EOL_ALIGN if number of tokens used is odd, COMPOSABLE_EOL_SKIP_ALIGH if even
+    *p++ = COMPOSABLE_EOL_SKIP_ALIGN;
+    *p++ = 0;
+
+    // Set number of words used and check if it exceeds buffer size
+    buffer->data_used = ((uint32_t *) p) - buffer->data;
+    assert(buffer->data_used < buffer->data_max);
+
+    // Set buffer status to be ready for use
+    buffer->status = SCANLINE_OK;
+}
+
+// Set block size for checkerboard demo based on potentiometer input
+uint16_t setBlockSize() {
+    adc_select_input(0);
+    uint16_t pot_raw = adc_read();
+    uint8_t pot = round(5 * (float)pot_raw / (1 << 12));
+
+    switch(pot) {
+        case 1: 
+            return 64;
+        case 2: 
+            return 32;
+        case 3: 
+            return 16;
+        case 4: 
+            return 8;
+        case 5: 
+            return 4;
+        default: // Input = 0 
+            return 128;
+    }
+}
+
+// Determine if square in checkerboard demo should be black
+bool blackSquare(uint16_t x, uint16_t y, uint16_t block_size) {
+    return (y % (2*block_size) < block_size && x % (block_size / 2) < (block_size/4)) || (y % (2*block_size) >= block_size && x % (block_size / 2) >= (block_size/4));
+}
+
+// Write pixel data for checkerboard demo
+void draw_checkerboard(scanvideo_scanline_buffer_t *buffer) {
+
+    uint16_t w_blocks = vga_mode.width/4;
+    uint16_t y = scanvideo_scanline_number(buffer->scanline_id);
+
+    uint16_t *p = (uint16_t *) buffer->data;
+
+    uint16_t block_size = setBlockSize();
+
+    for(int x = 0; x < w_blocks; x++) {
+        if(blackSquare(x, y, block_size)) {
+            draw_block(p, 0, 0, 0);
+        } else {
+            draw_block(p, 0x1f, 0x1f, 0x1f);
+        }
+        p += 3;
+    }
+
+    // 32 * 3, so we should be word aligned
+    assert(!(3u & (uintptr_t) p));
+
+    // Black pixel to end line (required to prevent color from bleeding into blanking)
+    *p++ = COMPOSABLE_RAW_1P;
+    *p++ = 0;
+    // End of line with alignment padding
+    // Use COMPOSABLE_EOL_ALIGN if number of tokens used is odd, COMPOSABLE_EOL_SKIP_ALIGH if even
+    *p++ = COMPOSABLE_EOL_SKIP_ALIGN;
+    *p++ = 0;
+
+    // Set number of words used and check if it exceeds buffer size
+    buffer->data_used = ((uint32_t *) p) - buffer->data;
+    assert(buffer->data_used < buffer->data_max);
+
+    // Set buffer status to be ready for use
+    buffer->status = SCANLINE_OK;
+}
+
+// Write pixel data for sinusoidal demo
 void draw_sine(scanvideo_scanline_buffer_t *buffer) {
 
-    uint16_t width = vga_mode.width;
-    uint16_t w_blocks = width/4;
-    uint16_t height = vga_mode.height;
-
+    uint16_t w_blocks = vga_mode.width/4;
     uint16_t y = scanvideo_scanline_number(buffer->scanline_id);
 
     uint16_t *p = (uint16_t *) buffer->data;
@@ -197,8 +272,6 @@ void core1_func() {
     // Values to control frame updates/toggling demos
     static uint32_t last_frame_num = 0;
     static uint8_t frame_count = 0;
-    //static uint16_t toggle_count = 0;
-    //static bool drawSine = false;
 
     while (true) {
 
@@ -215,21 +288,13 @@ void core1_func() {
         if(frame_num != last_frame_num) {
             last_frame_num = frame_num;
             frame_count++;
-            //toggle_count++;
-
-            /*
-            // Toggle demo every 900 frames (15 seconds)
-            if(toggle_count >= 900) {
-                drawSine = !drawSine;
-                toggle_count = 0;
-            }
-            */
         
             if(frame_count % i_frame == 0) {
                  i += i_inc;
                  frame_count = 0;
             }
 
+            // Keep offset value low so that sinusoidal demo does not struggle to keep up
             if(i >= 10000) {
                 i = 0;
             }
@@ -238,14 +303,28 @@ void core1_func() {
         // Select demo based on potentiometer input
         adc_select_input(1);
         uint16_t pot_raw = adc_read();
-        uint8_t pot = round((float)pot_raw / (1 << 12));
-    
+        uint8_t pot = round(3*(float)pot_raw / (1 << 12));
+
         // Draw pixels to buffer
-        if(pot == 1) {
-            draw_sine(scanline_buffer);
-        } else {
-            draw_checkerboard(scanline_buffer);
+        /*
+        switch(pot) {
+            case 2:
+                draw_box(scanline_buffer);
+            case 1:
+                draw_checkerboard(scanline_buffer);
+            default: // Input = 0
+                draw_sine(scanline_buffer);
         }
+        */
+       if(pot == 3) {
+            draw_pattern(scanline_buffer);
+       } else if (pot == 2) {
+            draw_box(scanline_buffer);
+       } else if (pot == 1) {
+            draw_checkerboard(scanline_buffer);
+       } else {
+            draw_sine(scanline_buffer);
+       }
         // Pass buffer to scanvideo code
         scanvideo_end_scanline_generation(scanline_buffer);
     }
@@ -256,8 +335,8 @@ int main(void) {
     sem_init(&video_initted, 0, 1);
     // Initialize ADC for potentiometer
     adc_init();
-    adc_gpio_init(26);
-    adc_gpio_init(27);
+    adc_gpio_init(26); // ADC input 0
+    adc_gpio_init(27); // ADC input 1
     // Run code on core 1
     multicore_launch_core1(core1_func);
     // Wait for video initialization to complete
